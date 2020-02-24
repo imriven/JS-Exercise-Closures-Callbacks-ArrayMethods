b09a91701df4d62d82ea8b2350dacd76a00b5897
Christina Harris <christinathedesigner@outlook.com>
2020-02-24 15:29:16 -0800

    complete exercise seven

--- index.js
+++ index.js
@@ -200,9 +200,23 @@ function processDuplicateFree(list, callback) {
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+/*
+function getFullNames(runners) {
+  let results = [];
+  function formatRunner(runner) {
+    results.push(`${runner.last_name}, ${runner.first_name}`)
+  }
+  runners.forEach(formatRunner);
+  return results;
 }
+*/
+
+function getFullNames(runners) {
+  let results = [];
+  runners.forEach(runner => results.push(`${runner.last_name}, ${runner.first_name}`));
+  return results;
+}
+
 
 /**
  * ### Challenge `firstNamesAllCaps`
