50f29a074797c9632691288235ad9c647d7faa10
Christina Harris <christinathedesigner@outlook.com>
2020-02-24 20:06:10 -0800

    complete last one

--- index.js
+++ index.js
@@ -323,8 +323,14 @@ function counterMaker() {
  * counter() // should return 0
  * etc
 */
-function counterMakerWithLimit(/* CODE HERE */) {
-  /* CODE HERE */
+function counterMakerWithLimit(maxValue) {
+  let count = 0;
+  return () => {
+    if (count > maxValue) {
+      count = 0;
+    }
+    return count++
+  }
 }
 
 /////////////// END OF CHALLENGE ///////////////
